0b2106c54d4e7f72c88cd1b8d6eac34fa8c6e6d4
rikakohirata <rikako.hirata88@gmail.com>
2023-05-11 13:35:59 +0900

    ルーティングの追加

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +34,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // ブログ
+    Route::resource('/blogs', BlogController::class)
+        ->names(['index'=>'blog.index',
+                'create' => 'blog.create',
+                'store' => 'blog.store',
+                'destroy' => 'blog.destroy',
+                'edit' => 'blog.edit',
+                'update' => 'blog.update',
+                'show' => 'blog.show',
+            ]);
 });
 
 require __DIR__.'/auth.php';
