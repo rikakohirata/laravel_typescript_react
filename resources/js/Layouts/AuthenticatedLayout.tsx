c8ce1ba4a4c111d6a10499591498be2549eabc5a
rikakohirata <rikako.hirata88@gmail.com>
2023-05-11 13:35:42 +0900

    Blogリンクの追加

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -25,6 +25,9 @@ export default function Authenticated({ user, header, children }: PropsWithChild
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('blog.index')} active={route().current('blog.index')}>
+                                    Blog
+                                </NavLink>
                             </div>
                         </div>
 
