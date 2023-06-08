f04df7c534a0b80bfc1663783fd149917820977e
rikakohirata <rikako.hirata88@gmail.com>
2023-06-08 20:08:21 +0900

    リッチエディタの実装

--- resources/js/Pages/Form/MyEditor.tsx
+++ resources/js/Pages/Form/MyEditor.tsx
@@ -4,6 +4,11 @@ import { PageProps } from '@/types';
 import { useState } from 'react';
 import { Editor, EditorState, RichUtils, DraftEditorCommand } from 'draft-js';  // Draft.jsをインポート
 import 'draft-js/dist/Draft.css';  // Draft.cssをインポート
+import { 
+    Container,
+    Button,
+    Box
+} from '@mui/material';
 
 
 export default function MyEditor({ auth }: PageProps) {
@@ -14,9 +19,9 @@ export default function MyEditor({ auth }: PageProps) {
     );
 
     // RichUtilsのhandleKeyCommand
-    // Command + B で太字が適用される
-    // Command + I で斜体が適用される
-    // Command + U で下線が適用される
+    // Command + B で太字
+    // Command + I で斜体
+    // Command + U で下線
     const handleKeyCommand = (command: DraftEditorCommand) => {
         const newState = RichUtils.handleKeyCommand(editorState, command);
         if (newState) {
@@ -24,6 +29,17 @@ export default function MyEditor({ auth }: PageProps) {
             return "handled";
         }
         return "not-handled";
+    };
+
+    // ツールバー
+    const handleTogggleClick = (e: React.MouseEvent, inlineStyle: string) => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleInlineStyle(editorState, inlineStyle));
+    };
+    
+    const handleBlockClick = (e: React.MouseEvent, blockType: string) => {
+        e.preventDefault();
+        setEditorState(RichUtils.toggleBlockType(editorState, blockType));
     };
 
   return (
@@ -34,12 +50,28 @@ export default function MyEditor({ auth }: PageProps) {
       <Head title="Rich Text editor" />
 
       {/* リッチテキストエディタ */}
-      <Editor
-        placeholder="入力してください"
-        editorState={editorState}
-        onChange={setEditorState}
-        handleKeyCommand={handleKeyCommand}
-      />
+      <Container maxWidth="xl">
+          <Box sx={{ marginTop: 3 }}>
+              {/* ツールバー */}
+              <Button onMouseDown={(e) => handleBlockClick(e, "header-one")}>H1</Button>
+              <Button onMouseDown={(e) => handleBlockClick(e, "header-two")}>H2</Button>
+              <Button onMouseDown={(e) => handleBlockClick(e, "header-three")}>H3</Button>
+              <Button onMouseDown={(e) => handleBlockClick(e, "unstyled")}>Normal</Button>
+              <Button onMouseDown={(e) => handleTogggleClick(e, "BOLD")}>bold</Button>
+              <Button onMouseDown={(e) => handleTogggleClick(e, "ITALIC")}>italic</Button>
+              <Button onMouseDown={(e) => handleTogggleClick(e, "STRIKETHROUGH")}>strikthrough</Button>
+              <Button onMouseDown={(e) => handleBlockClick(e, "ordered-list-item")}>Ordered List</Button>
+              <Button onMouseDown={(e) => handleBlockClick(e, "unordered-list-item")}>Unordered List</Button>
+
+              {/* テキストエリア */}
+              <Editor
+                placeholder="入力してください"
+                editorState={editorState}
+                onChange={setEditorState}
+                handleKeyCommand={handleKeyCommand}
+              />
+        </Box>
+    </Container>
 
 
     </AuthenticatedLayout>
