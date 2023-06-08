d266983fe8d018b408ed3a064958ebd8a58273db
rikakohirata <rikako.hirata88@gmail.com>
2023-06-08 14:25:20 +0900

    コンポーネントとして別ファイルに移動

--- resources/js/Pages/Item/Index.tsx
+++ resources/js/Pages/Item/Index.tsx
@@ -1,81 +1,24 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { useState } from 'react';
 import { PageProps } from '@/types';
-import { 
-    Container,
-    Box,
-    Typography,
-    Button,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Paper,
-} from '@mui/material';  // Material UIをインポート
+import Item from '@/types/type';
+import Pagination from './Pagination';
 
-// Item型を定義
-type Item = {
-    id: number; 
-    item_name: string; 
-    category: string; 
-    price: number; 
-};
 
-export default function Index({ auth, items }: PageProps<{ items: Item[] }>) {
+
+export default function Index({ auth }: PageProps) {
+    const [items, setItems] = useState<Item[]>([]);
+
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={<h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Item</h2>}
         >
             <Head title="Item" />
-
-            {/* 商品一覧を表示 */}
-            <Container maxWidth="lg">
-                <Box sx={{ 
-                        mt: '64px',  // margin-top
-                        pb: '16px',  // padding-bottom
-                        borderBottom: 1,
-                        borderColor: 'grey.500',
-                        display: "flex",
-                        justifyContent: 'space-between',
-                }}>
-                    <Typography component="h1" variant="h4">商品一覧</Typography> 
-                    <Button variant="contained" sx={{ backgroundColor: '#4B8F8F'}}>新規登録</Button> 
-                </Box>
-                <Box sx={{ marginTop: 8 }}>
-                    <TableContainer component={Paper}>
-                        <Table sx={{ 
-                                    minWidth: 650,
-                                    p: 10  // padding
-                                    }} aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell>No</TableCell>
-                                    <TableCell align="left">商品名</TableCell>
-                                    <TableCell align="left">カテゴリー</TableCell>
-                                    <TableCell align="left">金額</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {items.map((item: Item ) => (
-                                    <TableRow
-                                        key={item.item_name}
-                                        sx={{ 
-                                            '&:last-child td, &:last-child th': { border: 0 } 
-                                    }}>
-                                        <TableCell component="th" scope="row">{item.id}</TableCell>
-                                        <TableCell align="left">{item.item_name}</TableCell>
-                                        <TableCell align="left">{item.category}</TableCell>
-                                        <TableCell align="left">{item.price}</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Box>
-            </Container>
+            
+            {/* ページネーションコンポーネント */}
+            <Pagination items={items} />
         </AuthenticatedLayout>
     );
 }
